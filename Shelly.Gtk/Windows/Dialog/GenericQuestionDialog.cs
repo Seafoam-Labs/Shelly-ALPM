90f47e528672eed441765aba9a371ff2b4a9cdf4
Aaron Creor <aaroncreor@gmail.com>
2026-03-14 22:41:57 -0700

    Avoid horizontal scrolling in update confirmation

--- Shelly.Gtk/Windows/Dialog/GenericQuestionDialog.cs
+++ Shelly.Gtk/Windows/Dialog/GenericQuestionDialog.cs
@@ -1,4 +1,5 @@
 using Gtk;
+using Pango;
 using Shelly.Gtk.UiModels;
 
 namespace Shelly.Gtk.Windows.Dialog;
@@ -21,28 +22,43 @@ public static class GenericQuestionDialog
         titleLabel.AddCssClass("title-4");
         box.Append(titleLabel);
 
-        var messageLabel = Label.New(string.Empty);
-        messageLabel.SetHalign(Align.Start);
-        messageLabel.SetXalign(0);
-        messageLabel.SetJustify(Justification.Left);
-        messageLabel.SetWrap(!e.UseMonospaceMessage);
+        Widget messageWidget;
 
         if (e.UseMonospaceMessage)
         {
-            messageLabel.SetMarkup($"<tt>{GLib.Markup.EscapeText(e.Message)}</tt>");
+            var messageBox = Box.New(Orientation.Vertical, 2);
+            messageBox.SetHalign(Align.Fill);
+            messageBox.SetHexpand(true);
+
+            foreach (var line in e.Message.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries))
+            {
+                var lineLabel = Label.New(string.Empty);
+                lineLabel.SetHalign(Align.Fill);
+                lineLabel.SetHexpand(true);
+                lineLabel.SetXalign(0);
+                lineLabel.SetJustify(Justification.Left);
+                lineLabel.SetEllipsize(EllipsizeMode.End);
+                lineLabel.SetMarkup($"<tt>{GLib.Markup.EscapeText(line)}</tt>");
+                messageBox.Append(lineLabel);
+            }
+
+            messageWidget = messageBox;
         }
         else
         {
-            messageLabel.SetText(e.Message);
+            var messageLabel = Label.New(e.Message);
+            messageLabel.SetHalign(Align.Start);
+            messageLabel.SetXalign(0);
+            messageLabel.SetJustify(Justification.Left);
+            messageLabel.SetWrap(true);
+            messageWidget = messageLabel;
         }
 
         var scrolledWindow = new ScrolledWindow();
-        scrolledWindow.SetPolicy(
-            e.UseMonospaceMessage ? PolicyType.Automatic : PolicyType.Never,
-            PolicyType.Automatic);
+        scrolledWindow.SetPolicy(PolicyType.Never, PolicyType.Automatic);
         scrolledWindow.SetMaxContentHeight(300);
         scrolledWindow.SetPropagateNaturalHeight(true);
-        scrolledWindow.SetChild(messageLabel);
+        scrolledWindow.SetChild(messageWidget);
         box.Append(scrolledWindow);
 
         var buttonBox = Box.New(Orientation.Horizontal, 8);
