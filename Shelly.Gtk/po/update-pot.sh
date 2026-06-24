79e1ab31518f198fcb214ed033e5babae183f043
utuhiro78 <34818411+utuhiro78@users.noreply.github.com>
2026-06-24 18:25:21 +0900

    Update to generate the new PO files

--- Shelly.Gtk/po/update-pot.sh
+++ Shelly.Gtk/po/update-pot.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-xgettext --no-location --keyword=T -o shelly-ui.pot --from-code=UTF-8 ../*.cs ../*/*.{cs,ui} ../*/*/*.{cs,ui} ../*/*/*/*.cs
+find .. -type f \( -name "*.cs" -or -name "*.ui" \) | \
+    xgettext --no-location --keyword=T -o shelly-ui.pot \
+            --from-code=UTF-8 -f -
+
+for f in *.po
+do
+    msgmerge --update --backup=none "$f" shelly-ui.pot
+done
